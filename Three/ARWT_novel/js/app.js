1dadbf68bd10894d4bf89bc7db97d5d58a600d0a
TakaI2 <110tic2z@gmail.com>
2021-03-25 16:47:17 +0900

    add PN_Noh_demo1

--- Three/ARWT_novel/js/app.js
+++ Three/ARWT_novel/js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/NovelGame.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/AR_Noh.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
