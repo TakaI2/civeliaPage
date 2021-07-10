53e84e3a0bca413ca32f494948416200caa78f60
TakaI2 <110tic2z@gmail.com>
2021-07-10 14:54:00 +0900

    ippituページに表示改良版のgpsゲームを追加

--- ippitsuSP/gps_test20210706/script.js
+++ ippitsuSP/gps_test20210706/script.js
@@ -15,10 +15,128 @@ let play = document.getElementById("play");
 let pause = document.getElementById("pause");
 // ここまでsound関係
 
-var mermaids = [];                                  // ★人魚の情報を入れる変数
+var CheckPoints = [
+    {
+        "name": "1_ElmnoMori",
+        "lat":43.071049,
+        "lng":141.347466,
+        "pointer":"img/CP1.png",
+        "img":"img/1.png",
+        "sound":"sound/Check1.mp3",
+        "voice":"sound/1_elm_forest.mp3"
+    },
+    {
+        "name": "2_Elm",
+        "lat":43.071211,
+        "lng":141.347070,
+        "pointer":"img/CP2.png",
+        "img":"img/2.png",
+        "sound":"sound/Check2.mp3",
+        "voice":"sound/2_elm.mp3"
+    },
+    {
+        "name": "3_Urai",
+        "lat":43.072247,
+        "lng":141.345256,
+        "pointer":"img/CP3.png",
+        "img":"img/3.png",
+        "sound":"sound/Check3.mp3",
+        "voice":"sound/3_urai.mp3"
+    },
+    {
+        "name": "4_hurukawa",
+        "lat":43.071165,
+        "lng":141.343563,
+        "pointer":"img/CP1.png",
+        "img":"img/1.png",
+        "sound":"sound/Check1.mp3",
+        "voice":"sound/4_hurukawa.mp3"
+    },
+    {
+        "name": "5_Clark",
+        "lat":43.070670,
+        "lng":141.343381,
+        "pointer":"img/CP2.png",
+        "img":"img/2.png",
+        "sound":"sound/Check2.mp3",
+        "voice":"sound/5_clark.mp3"
+    },
+    {
+        "name": "6_OldBuilding",
+        "lat":43.070854,
+        "lng":141.342474,
+        "pointer":"img/CP3.png",
+        "img":"img/3.png",
+        "sound":"sound/Check3.mp3",
+        "voice":"sound/6_oldbuilding.mp3"
+    },
+    {
+        "name": "7_Musium",
+        "lat":43.072691,
+        "lng":141.342391,
+        "pointer":"img/CP1.png",
+        "img":"img/1.png",
+        "sound":"sound/Check1.mp3",
+        "voice":"sound/7_musiam.mp3"
+    },
+    {
+        "name": "8_OnoPond",
+        "lat":43.074359,
+        "lng":141.341918,
+        "pointer":"img/CP2.png",
+        "img":"img/2.png",
+        "sound":"sound/Check2.mp3",
+        "voice":"sound/8_musiam.mp3"
+    },
+    {
+        "name": "9_SakshkotoniRiv",
+        "lat":43.074226,
+        "lng":141.341126,
+        "pointer":"img/CP3.png",
+        "img":"img/3.png",
+        "sound":"sound/Check3.mp3",
+        "voice":"sound/9_sksktnLi.mp3"
+    },
+    {
+        "name": "10_Popla",
+        "lat":43.075222,
+        "lng":141.336632,
+        "pointer":"img/CP1.png",
+        "img":"img/1.png",
+        "sound":"sound/Check1.mp3",
+        "voice":"sound/10_popla.mp3"
+    },
+    {
+        "name": "11_LuinGurden",
+        "lat":43.058824,
+        "lng":141.332275,
+        "pointer":"img/CP2.png",
+        "img":"img/2.png",
+        "sound":"sound/Check2.mp3",
+        "voice":"sound/11_ruin.mp3"
+    },
+    {
+        "name": "ModelBurn",
+        "lat":43.081698,
+        "lng":141.339938,
+        "pointer":"img/CP3.png",
+        "img":"img/3.png",
+        "sound":"sound/Check3.mp3",
+        "voice":"sound/11_ruin.mp3"
+    },
+    {
+        "name": "Enyu",
+        "lat":43.081212,
+        "lng":141.341672,
+        "pointer":"img/CP1.png",
+        "img":"img/1.png",
+        "sound":"sound/Check1.mp3",
+        "voice":"sound/11_ruin.mp3"
+    }
+];                                  // ★人魚の情報を入れる変数
 var captured = [];                                  // ★人魚を捕獲済みか否かを入れる変数
-loadMermaids();                                     // ★人魚の情報を読み込む
 var getImg = document.getElementById("getImg");     // ★img要素の取得
+// loadCheckPoints();                                     // ★人魚の情報を読み込む
 
 // GPS センサの値が変化したら何らか実行する geolocation.watchPosition メソッド
 navigator.geolocation.watchPosition( (position) => {
@@ -27,17 +145,22 @@ navigator.geolocation.watchPosition( (position) => {
     var accu = position.coords.accuracy;            // 緯度・経度の精度を取得
     showMyPos(lat, lng);                            // showMyPos 関数を実行
     calcDistance(lat, lng);
+    console.log(lat + "," + lng);
 });
- 
+
 // 自分の位置を表示する showMyPos 関数
 function showMyPos(lat, lng) {
     var myPos = new google.maps.LatLng(lat, lng);   // Googleマップの LatLng オブジェクトを作成
     gmap.setCenter(myPos);                          // gmap の中心を myPos の位置にする
     mark.setPosition(myPos);                        // mark の位置を myPos にする
 }
- 
+
 // 地図の初期化
 function initMap() {
+    var start = document.getElementById("StartButtonDiv");
+    start.hidden = true;
+    var mainDiv = document.getElementById("mainDiv");
+    mainDiv.hidden = false;
     // 1回だけ現在位置を測定する getCurrentPosition メソッド
     navigator.geolocation.getCurrentPosition( (position) => {
         var lat = position.coords.latitude;         // 緯度を取得
@@ -51,39 +174,41 @@ function initMap() {
             map: gmap,                              // gmap の上に表示する
             position: initPos,                      // initPos の位置に
         });
-        placeMermaids();                            // ★人魚を配置する
+        placeCheckPoints();                            // ★人魚を配置する
     }, (error) => {                                 // エラー処理（今回は特に何もしない）
     }, {
         enableHighAccuracy: true                    // 高精度で測定するオプション
     });
 }
- 
-// ★人魚の情報を読み込む loadMermaids 関数
-function loadMermaids() {
-    var req = new XMLHttpRequest();                 // サーバのファイルを読む XMLHttpRequest
-    req.addEventListener("readystatechange", () => {        // 準備状態に変化があった時の処理
-        if(req.readyState === 4 && req.status === 200) {    // データ受信が正常に完了したら
-            mermaids = JSON.parse(req.responseText);        // 読み込んだJSONデータを整形して人魚データに入れる
-        }
-    });
-    req.open("GET", "mermaids.json");               // リクエストを設定
-    req.send();                                     // リクエストを送る
-}
- 
-// ★人魚を地図上に配置する placeMermaids 関数
-function placeMermaids() {
+
+
+// // ★人魚の情報を読み込む loadMermaids 関数
+// function loadCheckPoints() {
+//     var req = new XMLHttpRequest();                 // サーバのファイルを読む XMLHttpRequest
+//     req.addEventListener("readystatechange", () => {        // 準備状態に変化があった時の処理
+//         if(req.readyState === 4 && req.status === 200) {    // データ受信が正常に完了したら
+//             CheckPoints = JSON.parse(req.responseText);        // 読み込んだJSONデータを整形して人魚データに入れる
+//         }
+//     });
+//     req.open("GET", "mermaids.json");               // リクエストを設定
+//     req.send();                                     // リクエストを送る
+// }
+
+
+// ★人魚を地図上に配置する placeCheckPoints 関数
+function placeCheckPoints() {
     var mermaidMark = [];                           // 人魚マーカーの配列
-    for(var i = 0; i < mermaids.length; i++) {      // 全ての人魚について
-        var pos = new google.maps.LatLng(mermaids[i].lat, mermaids[i].lng); // 人魚の位置を設定
+    for(var i = 0; i < CheckPoints.length; i++) {      // 全ての人魚について
+        var pos = new google.maps.LatLng(CheckPoints[i].lat, CheckPoints[i].lng); // 人魚の位置を設定
         var img = {                                 // 画像の設定
-            url: mermaids[i].img,                   // 画像ファイル名
+            url: CheckPoints[i].pointer,                   // 画像ファイル名
             scaledSize: new google.maps.Size(60, 60)    // 画像を縮小表示
         };
         mermaidMark[i] = new google.maps.Marker({   // 人魚のマーカーを作成
             map: gmap,                              // gmap の上に表示する
             position: pos,                          // pos の位置に
             icon: img,                              // アイコンを設定
-            title: mermaids[i].name                 // タイトルを設定
+            title: CheckPoints[i].name                 // タイトルを設定
         });
         captured[i] = false;                        // 捕獲済み状態を全てfalseにする
     }
@@ -93,24 +218,28 @@ function placeMermaids() {
 function calcDistance(lat, lng) {
     var distance = [];                              // 距離を入れる配列
     var myPos = new google.maps.LatLng(lat, lng);   // Googleマップの LatLng オブジェクトを作成
-    for(var i = 0; i < mermaids.length; i++) {      // 全ての人魚について
-        var pos = new google.maps.LatLng(mermaids[i].lat, mermaids[i].lng);                 // 人魚の位置を設定
+    for(var i = 0; i < CheckPoints.length; i++) {      // 全ての人魚について
+        var pos = new google.maps.LatLng(CheckPoints[i].lat, CheckPoints[i].lng);                 // 人魚の位置を設定
         distance[i] = google.maps.geometry.spherical.computeDistanceBetween(myPos, pos);    // 距離を求める
         // 捕獲の判定と捕獲した時のエフェクト
         if(distance[i] < 20 && captured[i] === false) {         // 距離が20m未満、かつ、まだ捕獲していないなら
-            var music = new Audio(mermaids[i].sound);　　　　　　 // music変数をさくせい
+            var music = new Audio(CheckPoints[i].sound);　　　　　　 // music変数をさくせい
             music.play();                                       // 音を流す            
             captured[i] = true;                                 // 捕獲済にする
             get.hidden = false;                              // img要素を表示
-            mapDiv.hidden = true;                               // 地図を非表示
-            initPlayer(mermaids[i]);
+            mainDiv.hidden = true;                               // 地図を非表示
+            initPlayer(CheckPoints[i]);
             closeButton.addEventListener("click", () => {            // img要素がクリックされたら
                 track.pause();
                 get.hidden = true;                           // img要素を非表示
-                mapDiv.hidden = false;                          // 地図を表示
+                mainDiv.hidden = false;                          // 地図を表示
             });
         }
     }
+}
+
+function ReLoad(){
+    initMap();
 }
 
 // --------------ここからsound関係
